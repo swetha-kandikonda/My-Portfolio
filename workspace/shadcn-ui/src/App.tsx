eeb3081d47d9f8bef5701141420e2ff5e375cf5e
swetha-kandikonda <swethagayathri1964@gmail.com>
2025-09-17 10:32:10 +0530

    deployment

--- workspace/shadcn-ui/src/App.tsx
+++ workspace/shadcn-ui/src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Removed BrowserRouter from imports
 import Index from './pages/Index';
 import NotFound from './pages/NotFound';
 
@@ -11,12 +11,11 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      {/* BrowserRouter was removed from here */}
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
